5946ed4c369f8540f25698db38377da96ffbc7cd
bmanjuu <bettyofung@gmail.com>
2016-02-19 17:36:57 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,79 @@
+# require 'pry' 
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card = rand(1..11)
+  card
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  answer = gets.chomp
+  answer 
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  card_total = 0 
+  2.times do 
+    card_total += deal_card
+  end 
+  display_card_total(card_total)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
+end 
+
+def hit?(current_card_total)
+  # play = true 
+  # while current_card_total < 21 && play
+    prompt_user
+    answer = get_user_input
+
+    if answer == 's'
+      play = false
+    elsif answer == 'h'
+      # binding.pry
+      # deal_card
+      current_card_total += deal_card
+    else
+      invalid_command
+    end
+  # end
+  # binding.pry
+  current_card_total
 end
+
+# hit?(7)
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  x = initial_round
+  total = 0 
+  until total > 21
+    total += hit?(x)
+    display_card_total(total)
+  end
+  end_game(total)
 end
     
